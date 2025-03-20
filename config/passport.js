907c306e2aa20cbb726f56cd1b8357111b068d4b
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-03-20 13:58:16 -0600

    Added Functionality to authController

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const {PrismaClient} = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            const user = await Prisma.user.findUnique({where: {username}});
+            if (!user) return done(null, false, {message: 'Incorrect Username'});
+            const isValid = await bcrypt.compare(password, user.password);
+            if (!isValid) return done(null, false, {message: 'Incorrect Password'});
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+module.exports = passport;
