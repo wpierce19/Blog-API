da26bd818898cd5d512cfa70fbfa3dac9df41255
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-04-01 16:48:32 -0600

    fixed passport bug when logging in

--- config/passport.js
+++ config/passport.js
@@ -5,18 +5,32 @@ import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-passport.use(new LocalStrategy(
-    async (username, password, done) => {
-        try {
-            const user = await prisma.user.findUnique({where: {username}});
-            if (!user) return done(null, false, {message: 'Incorrect Username'});
-            const isValid = await bcrypt.compare(password, user.password);
-            if (!isValid) return done(null, false, {message: 'Incorrect Password'});
-            return done(null, user);
-        } catch (error) {
-            return done(error);
+passport.use(
+        new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password",
+        },
+        async (email,password,done) => {
+            try {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: email.toLowerCase(),
+                    },
+                });
+                if (!user) {
+                    return done(null, false, {message: "Email Not Found"});
+                }
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return done(null, false, {message: "Incorrect Password"});
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err);
+            }
         }
-    }
-));
+    )
+);
 
 export default passport;
